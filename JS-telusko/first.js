33d401045e6c50a8da3a2f52fd2554903d2967ad
Priyanka B <89992324+bpriya22@users.noreply.github.com>
2023-03-02 21:34:53 +0530

    added
    JS notes..

---
+++ JS-telusko/first.js
@@ -0,0 +1,93 @@
+// console.log("hello world...@@")
+
+// let radius="priya"
+// let pi=3.142
+// const area=pi*radius*radius
+// console.log( typeof area);
+// console.log(typeof pi);
+
+
+//DATATYPES
+
+//1]NUMBER
+// let date=8   //number
+// let user="priya"  //string
+
+// let num1=25467623746245
+// console.log(num1*25);
+// console.log(typeof num1);
+// console.log(typeof user);
+
+// let num2=0xff  //hexadecimal
+// console.log(num2);
+
+// let num3=1.5e12   //exponential--> 1.5*10^12
+// console.log(num3);
+
+// let num4=10_00_00_00_000  //easy to know how many zeros are their.
+// console.log(num4);
+
+// let num5=5/0; //infinit (positive infinity)
+// console.log(num5);
+
+// let num6=-5/0;
+// console.log(num6);
+
+// console.log(Number.MAX_VALUE*10);
+
+// let num33=1232323235233434333336   //loosing precision(unable to see the last digit.)
+// console.log(num33);
+
+//BIGINT
+// let num34=1232323235233434333336n  //with bigint we'll not loose precision (we'll get the last digit too..)
+// console.log(num34);
+
+// let num35=1232323235233434333336n
+// console.log(num35+2);//TypeError: Cannot mix BigInt and other types, use explicit conversions
+ //at Object.
+
+// let num36=1232323235233434333336n
+// console.log(num36+2n);
+
+//2]STRING
+// STRING OPERATIONS
+// let fname="priya"
+// let lname="anka"
+// user=fname+lname
+// console.log(user);
+
+//3] BOOLEAN
+// let bool=5>6
+// console.log(bool);
+// console.log(typeof bool);
+
+//4] NULL
+// let user=null
+// console.log(user);   //'null'
+// console.log(typeof null);  //'object'
+
+//5] UNDEFINED
+// let user
+// console.log(user);
+// console.log(typeof user);
+
+//NAN  /*interview queston*/
+// let num=5
+// console.log(5/"priya");   //'NAN'-->NOT A NUMBER-which is not a number
+// console.log(typeof(5/"priya"));  //'number'
+
+
+
+//TYPE CONVERSION AND TYPE COERCION
+
+//1] TYPE CONVERSION
+let num=String(6)       //Explicit conversion
+console.log(num, typeof num);
+
+//2] TYPE COERCION
+
+
+
+
+
+
